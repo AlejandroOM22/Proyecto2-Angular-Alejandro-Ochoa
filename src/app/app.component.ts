699303bd142fec79b322b0637c5ec057b387d028
Alejandro Ochoa <alejandroairton549@gmail.com>
2024-06-11 06:05:52 +0200

    version-parcial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BodyComponent } from './pagna/body/body.component';
+import { NavbarComponent } from './pagna/navbar/navbar.component';
+import { FooterComponent } from './pagna/footer/footer.component';
+import { SliderComponent } from './pagna/slider/slider.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BodyComponent, NavbarComponent, FooterComponent, SliderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { NavbarComponent } from './pagna/navbar/navbar.component';
+import { Component } from '@angular/core';
+import { BodyComponent } from './pagna/body/body.component';
+import { FooterComponent } from './pagna/footer/footer.component';
+import { SliderComponent } from './pagna/slider/slider.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: 'navbar', component:NavbarComponent
+    },
+    {
+        path: 'body', component:BodyComponent
+    },
+    {
+        path: 'footer', component:FooterComponent
+    },
+    {
+        path: 'slider', component:SliderComponent
+    }
+];
+
